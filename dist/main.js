955369751b1f1dbdb591a0cdc21a3401551ee7a1
Le Van Chuyen <duythi794@gmail.com>
2025-02-24 10:16:14 +0700

    Update

--- dist/main.js
+++ dist/main.js
@@ -4,4 +4,5 @@ const rectangle_1 = require("./rectangle");
 const rectangle = new rectangle_1.Rectangle(5, 5, 10, 20);
 console.log(rectangle.toString());
 console.log(rectangle.area());
+
 //# sourceMappingURL=main.js.map
